6789eb22eaa958fdf1aef944889c9e9bc632acdc
Dewayne VanHoozer <dvanhoozer@gmail.com>
2024-10-04 18:48:49 -0500

    Issue 1: Add error handling test for middleware
    This commit introduces a test to ensure middleware can handle errors.
    It adds an `ErrorMiddleware` class to simulate network issues, helping
    to assess robustness in error scenarios when interacting with external
    APIs. This enhancement supports future debugging and reliability.

--- test/ai_client/middleware_test.rb
+++ test/ai_client/middleware_test.rb
@@ -41,6 +41,38 @@ class AiClient::MiddlewareTest < Minitest::Test
     assert_equal 'Generated text two one', result
   end
 
+  # TODO: how robust is the error handling?  Not very!
+  def test_middleware_should_handle_errors
+    AiClient.clear_middlewares
+    AiClient.use(ErrorMiddleware)
+    local_client = AiClient.new(@model)
+
+    # SMELL:  This mock does not call the middleware so does
+    #         not raise the fake error.
+    #         So, without the mock, we are hitting OpenAI
+    #
+    # mock_client = mock()
+    # mock_client.expects(:chat).raises(RuntimeError, "Simulated network issue")
+    # local_client.instance_variable_set(:@client, mock_client)
+
+    assert_raises(RuntimeError) do
+      local_client.chat('I am testing do not respond.')
+    end
+  end
+
+  def test_middleware_can_be_added_after_client_creation
+    AiClient.clear_middlewares
+    local_client = AiClient.new(@model)
+
+    mock_client = mock()
+    mock_client.expects(:chat).returns(OpenStruct.new(data: { 'choices' => [{ 'message' => { 'content' => 'Generated text' } }] }))
+    local_client.instance_variable_set(:@client, mock_client)
+
+    AiClient.use(TestMiddleware)
+
+    result = local_client.chat([{ role: 'user', content: 'Hello' }])
+    assert_equal "Generated text - Processed by TestMiddleware", result
+  end
 
 end
 
@@ -72,3 +104,11 @@ class Middleware2
     result
   end
 end
+
+
+class ErrorMiddleware
+  def self.call(client, _next_middleware, *_args)
+    raise RuntimeError.new("Simulated network issue")
+  end
+end
+
